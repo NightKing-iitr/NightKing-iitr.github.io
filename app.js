8121d6611b3598d19b9014381a4b8387c8f6d03d
NightKing-iitr <ankitadityasingh786@gmail.com>
2018-06-06 18:31:00 +0530

    set the environment port in app.js file

--- app.js
+++ app.js
@@ -80,8 +80,6 @@ app.use('/', routes);
 app.use('/users', users);
 
 // Set Port
-app.set('port', (process.env.PORT || 3000));
-
-app.listen(app.get('port'), function(){
-	console.log('Server started on port '+app.get('port'));
+var server = app.listen(process.env.PORT || 8080, function(){
+  console.log('Server started on port ' + server.address().port);
 });
